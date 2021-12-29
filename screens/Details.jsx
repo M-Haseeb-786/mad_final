ee170db91dc201ba1ef9024a771bb9272888c93d
M-Haseeb-786 <fa18-bcs-069@cuilahore.edu.pk>
2021-12-29 16:07:46 +0500

    first commit

---
+++ screens/Details.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, SectionList, TextInput, Button } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+
+
+const Details = ({ navigation, route }) => {
+    const [data, setData] = useState([]);
+    const [oldData, setOldData] = useState([]);
+    const [search, setSearch] = useState("");
+    const [chaps, setChaps] = useState([])
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        let { book } = route.params;
+        let chapters = book.split("&");
+        chapters = chapters.filter(p => p !== "\r\n\r\n")
+        chapters = chapters.map(p => p.split("$"))
+        let obj = [];
+        chapters.map((chapter, index) => index !== 0 && obj.push({ title: chapter[0], data: [chapter[1]] }))
+        setChaps(obj)
+        setData(chapters)
+    }, [])
+
+    const handleSearch = () => {
+        if(!search) return setError("Please enter and item to search")
+        let temp = chaps.filter( ch => ch.title.indexOf(search) !== -1 || ch.data.join("").indexOf(search) !== -1)
+        if(temp.length < 1) return setError("No items were found")
+        setOldData(chaps)
+        setChaps(temp)
+    }
+
+    const handleReset = () => {
+        if(oldData && oldData.length)
+            setChaps(oldData);
+    }
+
+    const Item = ({ title }) => (
+        <View style={styles.item}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      );
+
+    return (
+        <View>
+            <TextInput 
+                value={search}
+                placeholder='Search for a book'
+                onChangeText={text => {setSearch(text); setError("")}}
+                style={styles.textInput}
+            />
+            <Button 
+                title="Search"
+                style={styles.btn}
+                onPress={handleSearch}
+            />
+            <Button 
+                title="Reset"
+                style={styles.btn}
+                onPress={handleReset}
+            />
+            <Text>{ error && <Text style={styles.err}>{error}</Text> }</Text>
+            <SectionList
+                sections={chaps}
+                keyExtractor={(item, index) => item + index}
+                renderItem={({ item }) => <Item title={item} />}
+                renderSectionHeader={({ section: { title } }) => (
+                    <Text style={styles.header}>{title}</Text>
+                )}
+            />
+        </View>
+    )
+}
+
+export default Details
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: StatusBar.currentHeight,
+      marginHorizontal: 16
+    },
+    item: {
+      backgroundColor: "#f9c2ff",
+      padding: 20,
+      marginVertical: 8
+    },
+    header: {
+      fontSize: 32,
+      backgroundColor: "#fff"
+    },
+    title: {
+      fontSize: 24
+    },
+    textInput:{
+        height:50,
+        padding: 10
+    },
+    err: {
+        color: "red",
+        fontWeight: 'bold',
+        marginTop:5
+    },
+  });
